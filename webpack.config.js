bd3a09d097a7674e60ba2645370e7732aa4841a6
uxueadrian <adrianuxuechavezmartinez@gmail.com>
2026-07-29 18:05:33 -0600

    chore: initialize project with Webpack 5, Babel, and React

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production"
+
+  return {
+    entry: "./src/main.jsx",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: isProduction ? "[name].[contenthash].js" : "[name].js",
+      clean: true,
+      publicPath: "/",
+    },
+    resolve: {
+      extensions: [".js", ".jsx"],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: "babel-loader",
+        },
+        {
+          test: /\.css$/,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : "style-loader",
+            "css-loader",
+            "postcss-loader",
+          ],
+        },
+        {
+          test: /\.(png|jpg|jpeg|gif|svg)$/,
+          type: "asset/resource",
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./public/index.html",
+        favicon: "./public/favicon.svg",
+      }),
+      ...(isProduction
+        ? [
+            new MiniCssExtractPlugin({
+              filename: "[name].[contenthash].css",
+            }),
+          ]
+        : []),
+    ],
+    devServer: {
+      port: 3000,
+      hot: true,
+      open: true,
+      historyApiFallback: true,
+    },
+    devtool: isProduction ? false : "eval-source-map",
+  }
+}
